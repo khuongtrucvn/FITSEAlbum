205eb372b46663858e555489e5b0956d09c82591
khuongtrucvn <supertruc2102@yahoo.com>
2018-10-12 12:30:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a8560p/fitsealbum/ImageAdapter.java
@@ -0,0 +1,46 @@
+package com.example.a8560p.fitsealbum;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context context;
+
+    public Integer[] images = {
+            R.drawable.img_001, R.drawable.img_002, R.drawable.img_003
+    };
+
+    public ImageAdapter(Context c)
+    {
+        context = c;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return images[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(context);
+        imageView.setImageResource(images[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        imageView.setLayoutParams(new GridView.LayoutParams(240,240));
+        return imageView;
+    }
+}
---
+++ app/src/main/java/com/example/a8560p/fitsealbum/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.a8560p.fitsealbum;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // setup ActionBar
+        actionBar = getSupportActionBar();
+
+        actionBar.setTitle("  FIT SE Album");
+        actionBar.setSubtitle("  Version 1.0");
+        actionBar.setIcon(R.drawable.ic_action_logo);
+
+        actionBar.setDisplayShowCustomEnabled(true);            // allow custom views to be shown
+        actionBar.setDisplayShowHomeEnabled(true);              // allow app icon – logo to be shown
+        actionBar.setHomeButtonEnabled(true);                   // needed for API14 or greater
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override  public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; add items to the action bar
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // return a SHARED intent to deliver an email
+    private Intent emailIntent() {
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "v.matos@csuohio.edu" });
+        intent.putExtra(Intent.EXTRA_SUBJECT, "subject here...");
+        intent.putExtra(Intent.EXTRA_TEXT, "this is the email-text to be sent...");
+
+        return intent;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    // user clicked a menu-item from ActionBar
+        int id = item.getItemId();
+        if (id == R.id.action_search) {
+            // perform SEARCH operations...
+            return true;
+        }
+        else if (id == R.id.action_share) {
+            startActivity( Intent.createChooser( emailIntent(), "Send EMAIL Using...") );
+            return true;
+        }
+
+        else if (id == R.id.action_about) {
+            // perform ABOUT operations...
+            return true;
+        }
+        else if (id == R.id.action_settings) {
+            // perform SETTING operations...
+            return true;
+        }
+        return false;
+    }
+}
